6cb9484a4c98b9037385cf2831feae1dd62cd3f9
Pranay Kr. Srivastava <pranjas@gmail.com>
2020-09-13 23:17:17 +0530

    Add back button handling from todolist screen
    The back button currently would always bring the user to
    login screen.
    1. Set loginActivity as the parent activity.
    2. Handle ActionBar onMenuOptions to set
    the activity result before destroying it.
    
    TODO:
    we would change this to only
    do this behaviour if a log out is requested by user.
    
    Signed-off-by: Pranay Kr. Srivastava <pranjas@gmail.com>

--- app/src/main/java/com/example/windup/ui/TodoListActivity.java
+++ app/src/main/java/com/example/windup/ui/TodoListActivity.java
@@ -6,14 +6,47 @@
 
 package com.example.windup.ui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.windup.Application;
+import com.example.windup.ApplicationConstants;
+import com.example.windup.R;
+import com.example.windup.ui.login.LoggedInUserView;
 
 public class TodoListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        displayLoggedInUser();
+    }
+
+    private void displayLoggedInUser() {
+        LoggedInUserView loggedInUserView = Application.getLoggedInUser();
+
+        if (loggedInUserView != null) {
+            setTitle("Welcome " + loggedInUserView.getDisplayName());
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                setResult(ActivityConstants.ACTIVITY_TODOLIST_RESULT, null);
+                finish();
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
